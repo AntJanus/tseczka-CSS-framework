10b89aab66bec70199685c120a2badf93ebebf27
Antonin Januska <antonin@antjanus.com>
2014-06-10 20:47:43 -0500

    Fixing gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var autoprefix = require('gulp-autoprefixer');
 var less = require('gulp-less');
 
 gulp.task('styles', function() {
-  gulp.src('base.less')
+  gulp.src(['base.less'])
     .pipe(autoprefix())
     .pipe(less())
     .pipe(gulp.dest('./dist/tseczka.css'))
   ;
 });
 
-gulp.task('default', 'styles', function(){
+gulp.task('default', ['styles'], function(){
   gulp.watch('./less/*.less', ['styles']);
 });
